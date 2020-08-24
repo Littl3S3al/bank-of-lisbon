065e8372ee6cc68772ab9f40ade308951ba14de3
Littl3S3al <lauseal@gmail.com>
2020-08-24 17:23:55 +0200

    small changes

--- js/footnotes.js
+++ js/footnotes.js
@@ -1,6 +1,8 @@
 const notes = [
     `Langa, M. 2018, ‘Moshekwa Langa’. Cité International des Arts, viewed 023 January 2020, < https://www.citedesartsparis.net/ en/moshekwa-langa>.`,
+
     `As summarised in the abstract, the following text is primarily based on open source research of online news, articles, videos, images and interviews I have followed in trying to understand what happened. It is neither impartial nor comprehensive, but a selective re-mediation of existing information. The attempts to reach city officials, employees and firemen, or locate building plans have mainly led to dead ends. `,
+    
     `Description of blast plan and preparation by Joe Brinkmann, director of Jet Demolition, which was contracted to demolish the Bank of Lisbon by the Gauteng Department of Infrastructure Development. ‘106-m high Bank of Lisbon implosion is one of the most complex projects ever undertaken by Jet Demolition’ 2019, Mining News, 01 December, viewed 20 December 2019, 
     <https://www.miningsafety.co.za/newscontent/10220/108-m-high-bank-of-lisbon-implosion-is-one-of-the-most-complex-projects-ever-undertaken-by-jet-demolition>.
     `
